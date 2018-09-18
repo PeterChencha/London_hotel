e483602ae94b379f118b2c333dcadc39e9674e66
extraordinaire1 <peter@chenchatech.com>
2018-09-18 18:01:00 +0300

    modify data with eloquent

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -70,6 +70,7 @@ class ClientController extends Controller
     public function show($client_id)
     {
         $data=[];
+        $data['client_id']=$client_id;
         $data['titles']= $this->titles;
         $data['modify']=1;
         $client_data=$this->client->find($client_id);
@@ -81,6 +82,52 @@ class ClientController extends Controller
         $data['city']=$client_data->city;
         $data['state']=$client_data->state;
         $data['email']=$client_data->email;
+        return view('client/form',$data);
+    }
+
+    public function modify(Request $request,$client_id ,Client $Client)//DEPENDANCIES INJECTION
+    {
+        $data=[];
+        $data['title']=$request->input('title');
+        $data['name']=$request->input('name');
+        $data['last_name']=$request->input('last_name');
+        $data['address']=$request->input('address');
+        $data['zip_code']=$request->input('zip_code');
+        $data['city']=$request->input('city');
+        $data['state']=$request->input('state');
+        $data['email']=$request->input('email');
+
+
+     if ( $request->isMethod('post')    ) {
+         //dd($data);
+         $this->validate(
+           $request,[
+             'name'=>'required | min:5',//MINIMUM CHARACTERS EXPECTED
+             'last_name'=>'required',
+             'address'=>'required',
+             'zip_code'=>'required',
+             'city'=>'required',
+             'state'=>'required',
+             'email'=>'required',
+           ]
+         );
+
+         $client_data=$this->client->find($client_id);
+
+         $client_data->title=$request->input('title');
+         $client_data->name=$request->input('name');
+         $client_data->last_name=$request->input('last_name');
+         $client_data->address=$request->input('address');
+         $client_data->zip_code=$request->input('zip_code');
+         $client_data->city=$request->input('city');
+         $client_data->state=$request->input('state');
+         $client_data->email=$request->input('email');
+
+         $client_data->save();
+         return redirect('clients');
+     }
+         $data['titles']= $this->titles;
+         $data['modify']=0;
         return view('client/form',$data);
     }
 }
