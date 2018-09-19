3fecacad88d4663f212a7093f66ae30354b5ade4
extraordinaire1 <peter@chenchatech.com>
2018-09-19 10:23:15 +0300

    relational

--- app/Reservation.php
+++ app/Reservation.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
+    public function client()
+    {
+      // code...
+        return $this->belongsTo('App\Client','client_id','id');
+    }
 }
